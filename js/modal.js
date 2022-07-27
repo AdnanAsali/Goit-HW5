4a363bb658950b526a7f84b17c50ef1a70efeb8d
Dev User <devuser@zifafs-MBP.lan>
2022-07-27 23:49:01 +0300

    first commit

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+
+    document.querySelector('.call-to-action').addEventListener("click", function(e) 
+    {
+        document.querySelector('.backdrop').classList.toggle("is-hidden");
+        document.querySelector('.modal').classList.toggle("is-hidden");
+    });
+
+
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
